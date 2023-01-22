d1ca16e20677ff3a8f2e16800c73349d4e668a0f
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-01-22 17:49:06 +0100

    Add files via upload

---
+++ Priprema_petlje1/Priprema_petlje1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_petlje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocetak = 1900;
+            int kraj = 2100;
+
+            do
+            {
+                if (pocetak % 100 == 0)
+                {
+                    if (pocetak % 400 == 0)
+                    {
+                        Console.WriteLine(pocetak + ". je prijestupna godina.");
+                    }
+                }
+                else if (pocetak % 4 == 0)
+                {
+                    Console.WriteLine(pocetak + ". je prijestupna godina.");
+                }
+
+                pocetak++;
+            } while (pocetak <= kraj);
+
+
+            Console.ReadKey();
+        }
+    }
+}
